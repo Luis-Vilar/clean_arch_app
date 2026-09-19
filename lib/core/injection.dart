2e3ee7c7bdc2b72ccc2f2215b3ce445e500d46fe
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 17:40:11 -0300

    style: rename folders and update imports

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,5 +1,5 @@
 import 'package:clean_arch_app/app/domain/interfaces/todo_repository_interface.dart';
-import 'package:clean_arch_app/app/infra/implementations/todo_repository.dart';
+import 'package:clean_arch_app/app/infra/repositories/todo_repository.dart';
 import 'package:get_it/get_it.dart';
 
 final injection = GetIt.instance;
