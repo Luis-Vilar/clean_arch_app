21883b11443bc57f7a5c176d7ff5a8931dc6f488
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 17:12:06 -0300

    feat: implement TodoRepository class for CRUD operations on todos

---
+++ lib/app/infra/implementations/todo_repository.dart
@@ -0,0 +1,28 @@
+import 'package:clean_arch_app/app/domain/entities/todo_entity.dart';
+import 'package:clean_arch_app/app/domain/interfaces/todo_repository_interface.dart';
+import 'package:clean_arch_app/app/infra/sources/local/todo_database.dart';
+import 'package:clean_arch_app/app/utils/result.dart';
+
+class TodoRepository implements TodoRepositoryInterface {
+  final _db = TodoDatabase();
+
+  @override
+  Future<Result<TodoEntity>> createTodo(TodoEntity todo) {
+    return _db.insertTodo(todo);
+  }
+
+  @override
+  Future<Result<TodoEntity>> deleteTodo(TodoEntity todo) {
+    return _db.deleteTodo(todo);
+  }
+
+  @override
+  Future<Result<List<TodoEntity>>> readAllTodos() {
+    return _db.getAllTodos();
+  }
+
+  @override
+  Future<Result<TodoEntity>> updateTodo(TodoEntity todo) {
+    return _db.updateTodo(todo);
+  }
+}
