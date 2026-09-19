0f595e4027ad013e48cd5010c37d27fc2bc9601c
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 17:12:20 -0300

    feat: update bootstrap function to initialize system settings and dependencies asynchronously

--- lib/core/bootstrap.dart
+++ lib/core/bootstrap.dart
@@ -1,13 +1,21 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:clean_arch_app/app/infra/utils/db_helper.dart';
+import 'package:clean_arch_app/core/injection.dart';
 import 'package:clean_arch_app/core/main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void bootstrap() {
   runZonedGuarded(
-    () {
+    () async {
       WidgetsFlutterBinding.ensureInitialized();
+      await SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+      await DbHelper.db;
+      initDependencyInjection();
 
       runApp(MainApp());
     },
