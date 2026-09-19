74d1935ce43795fe86ab17dfdc5347495920029d
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 17:11:31 -0300

    feat: add SqfliteFailure class to handle database-related failures

--- lib/app/utils/failures.dart
+++ lib/app/utils/failures.dart
@@ -2,3 +2,7 @@ sealed class Failure implements Exception {
   Failure({required this.message});
   String message;
 }
+
+final class SqfliteFailure extends Failure {
+  SqfliteFailure({required super.message});
+}
