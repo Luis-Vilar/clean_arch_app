74a22974f3d2c2a6b4077f4b715d7e4d25eacb3e
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 17:10:52 -0300

    feat: add TodoModel class to represent TodoEntity with mapping methods

---
+++ lib/app/infra/models/todo_model.dart
@@ -0,0 +1,39 @@
+import 'package:clean_arch_app/app/domain/entities/todo_entity.dart';
+
+final class TodoModel extends TodoEntity {
+  TodoModel({
+    required super.id,
+    required super.title,
+    required super.todo,
+    required super.createdAt,
+    required super.completed,
+  });
+
+  factory TodoModel.fromMap(Map<String, dynamic> map) => TodoModel(
+    id: map['id'],
+    title: map['title'],
+    todo: map['todo'],
+    createdAt: map['createdAt'] is String
+        ? DateTime.parse(map['createdAt'] as String)
+        : map['createdAt'] as DateTime,
+    completed: map['completed'] is int
+        ? map['completed'] == 1
+        : map['completed'] as bool,
+  );
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'title': title,
+    'todo': todo,
+    'createdAt': createdAt.toIso8601String(),
+    'completed': completed ? 1 : 0,
+  };
+
+  TodoEntity toEntity() => TodoEntity(
+    id: id,
+    title: title,
+    todo: todo,
+    createdAt: createdAt,
+    completed: completed,
+  );
+}
