9afd7f40c5bcdfe9c3b0f1e091b453c5dd467591
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 18:54:49 -0300

    refactor: update Failure class to remove Exception implementation and adjust SqfliteFailure

--- lib/app/shared/failures.dart
+++ lib/app/shared/failures.dart
@@ -1,8 +1,10 @@
-sealed class Failure implements Exception {
+sealed class Failure {
+  final String message;
+
   Failure({required this.message});
-  String message;
 }
 
-final class SqfliteFailure extends Failure {
-  SqfliteFailure({required super.message});
+final class SqfliteFailure implements Failure {
+  @override
+  String get message => 'No foi possível manipular a base de dados.';
 }
