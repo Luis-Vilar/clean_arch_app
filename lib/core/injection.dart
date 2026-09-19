605b9af6176ddafb883047d1ef1ab7b9e6e09a52
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-19 19:02:59 -0300

    refactor: register TodoDatabaseInterface in dependency injection

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch_app/app/domain/interfaces/todo_repository_interface.dart';
+import 'package:clean_arch_app/app/infra/data_sources/local/todo_database.dart';
 import 'package:clean_arch_app/app/infra/repositories/todo_repository.dart';
 import 'package:get_it/get_it.dart';
 
@@ -7,5 +8,8 @@ final injection = GetIt.instance;
 void initDependencyInjection() {
   injection.registerLazySingleton<TodoRepositoryInterface>(
     () => TodoRepository(),
+  );
+  injection.registerLazySingleton<TodoDatabaseInterface>(
+    () => TodoDatabaseSqfliteImplementation(),
   );
 }
